fc0acbad6f876ff354553a6be4b5746520a75d39
bamtolcompany <mean119@naver.com>
2025-12-16 23:11:15 +0900

    d

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // --- '로컬 채팅' 데이터 구조 ---
 const NICKNAMES_FILE = path.join(__dirname, 'nicknames.json');
